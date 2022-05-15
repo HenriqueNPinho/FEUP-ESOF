832ea9dab79b33b9f9d6b9dc62b8be872d0a49ef
up201904529 <up201904529@fe.up.pt>
2022-05-15 16:51:11 +0100

    algumas alterações de m3rd4

--- UNI_App-main/lib/view/Pages/ticket_page_view.dart
+++ UNI_App-main/lib/view/Pages/ticket_page_view.dart
@@ -3,6 +3,10 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image/image.dart';
 import 'package:uni/view/Pages/general_page_view.dart';
 import 'package:uni/view/Widgets/terms_and_conditions.dart';
+import 'package:uni/view/Pages/cancel_ticket_page.dart';
+
+var ticketNumber = null;
+var areaChoosen = null;
 
 class TicketPageView extends StatefulWidget {
   @override
@@ -14,18 +18,17 @@ class TicketPageViewState extends GeneralPageViewState {
   @override
   Widget getBody(BuildContext context) {
     final MediaQueryData queryData = MediaQuery.of(context);
-    return ListView(
-      children: <Widget>[
-        Container(
+    return ListView(children: <Widget>[
+      Container(
           //color: Colors.blueGrey,
-            margin: const EdgeInsets.all(15),
-            alignment: Alignment.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
+          margin: const EdgeInsets.all(15),
+          alignment: Alignment.topCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                   Container(
                     padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
                     child: Text('Senha'),
@@ -34,55 +37,142 @@ class TicketPageViewState extends GeneralPageViewState {
                     padding: EdgeInsets.fromLTRB(90, 10, 0, 10),
                     child: Text('Em espera'),
                   ),
-                  ],
-                ),
-                makeBoxCard('A', '025', '02'),
-                makeBoxCard('B', '025', '05'),
-                makeBoxCard('C', '025', '25'),
+                ],
+              ),
+              makeBoxCard('A', '025', '02'),
+              makeBoxCard('B', '025', '05'),
+              makeBoxCard('C', '025', '25'),
             ],
-            )
+          )),
+
+      Ink(
+        decoration: ShapeDecoration(
+          color: Theme.of(context).accentColor,
+          shape: CircleBorder(),
         ),
-
-        Ink(
-          decoration: ShapeDecoration(
-            color: Theme.of(context).accentColor,
-            shape: CircleBorder(),
+        child: IconButton(
+          icon: Icon(
+            Icons.question_mark_rounded,
+            color: Theme.of(context).primaryColor,
           ),
-          child: IconButton(
-            icon: Icon(
-              //AssetImage('assets/icon/question.png')
-              Icons.question_mark_rounded,
-              color: Theme.of(context).primaryColor,
-            ),
-            onPressed: (){ print('botao ? clicado');
-            },
-          ),
+          onPressed: () {
+            helpPopUp(context);
+          },
         ),
+      ),
 
-        makeBoxInf(),
+      makeBoxInf(),
 
-        //button 
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 40),
-          child:  ElevatedButton(
+      //button
+      Container(
+        margin: EdgeInsets.symmetric(horizontal: 40),
+        child: ElevatedButton(
             child: Text('Pedir Senha', style: TextStyle(fontSize: 20)),
-             style: ElevatedButton.styleFrom(
+            style: ElevatedButton.styleFrom(
               //shadowColor: Colors.black,
               elevation: 20,
-            ), 
-            /* style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(Size(220, 80)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        ))),
-            */
-            onPressed: () => showCustomDialog(context)
             ),
-        )
-      ]
-    );
+            onPressed: () => {
+                  if (ticketNumber == null && areaChoosen == null)
+                    {selectionError(context)}
+                  else
+                    {showCustomDialog(context)}
+                }),
+      )
+    ]);
   }
+}
+
+void helpPopUp(BuildContext context) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "help",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: Duration(milliseconds: 500),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 200,
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(20)),
+          child: Center(
+            child: DefaultTextStyle(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              child: Text('Desenrasca-te LOL'),
+            ),
+          ),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      if (anim.status == AnimationStatus.reverse) {
+        tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+      } else {
+        tween = Tween(begin: Offset(1, 0), end: Offset.zero);
+      }
+
+      return SlideTransition(
+        position: tween.animate(anim),
+        child: FadeTransition(
+          opacity: anim,
+          child: child,
+        ),
+      );
+    },
+  );
+}
+
+void selectionError(BuildContext context) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "Select",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: Duration(milliseconds: 500),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 200,
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          decoration: BoxDecoration(
+              color: Colors.white, borderRadius: BorderRadius.circular(20)),
+          child: Center(
+            child: DefaultTextStyle(
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              child: Text('Seleciona uma senha.'),
+            ),
+          ),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      if (anim.status == AnimationStatus.reverse) {
+        tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+      } else {
+        tween = Tween(begin: Offset(1, 0), end: Offset.zero);
+      }
+
+      return SlideTransition(
+        position: tween.animate(anim),
+        child: FadeTransition(
+          opacity: anim,
+          child: child,
+        ),
+      );
+    },
+  );
 }
 
 void showCustomDialog(BuildContext context) {
@@ -109,7 +199,7 @@ void showCustomDialog(BuildContext context) {
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                           color: Colors.black),
-                      child: Text('Confirmar senha?'),
+                      child: Text('Confirmar senha $areaChoosen$ticketNumber?'),
                     ),
                   ),
                   margin: EdgeInsets.fromLTRB(30, 50, 30, 50),
@@ -122,19 +212,14 @@ void showCustomDialog(BuildContext context) {
                         style: ButtonStyle(
                             minimumSize:
                                 MaterialStateProperty.all(Size(120, 40)),
-                            /*foregroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white),
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.red),
-                            shadowColor:
-                                MaterialStateProperty.all<Color>(Colors.black),*/
                             shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    //side: BorderSide(color: Colors.red)
-                                    ))),
-                        onPressed: () => null),
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ))),
+                        onPressed: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => TicketCancelPageView()))),
                   ),
                   Container(
                     child: ElevatedButton(
@@ -142,19 +227,11 @@ void showCustomDialog(BuildContext context) {
                         style: ButtonStyle(
                             minimumSize:
                                 MaterialStateProperty.all(Size(120, 40)),
-                            /*foregroundColor:
-                                MaterialStateProperty.all<Color>(Colors.white),
-                            backgroundColor:
-                                MaterialStateProperty.all<Color>(Colors.red),
-                            shadowColor:
-                                MaterialStateProperty.all<Color>(Colors.black),*/
                             shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    //side: BorderSide(color: Colors.red)
-                                    ))
-                                    ),
+                                RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              //side: BorderSide(color: Colors.red)
+                            ))),
                         onPressed: () => null),
                   ),
                 ]),
@@ -181,98 +258,96 @@ void showCustomDialog(BuildContext context) {
   );
 }
 
-Widget makeBoxInf(){
+Widget makeBoxInf() {
   return Container(
-          //color: Colors.blueAccent,
-          margin: const EdgeInsets.all(15),
-          alignment: Alignment.topCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 320,
-                height: 1,
-                margin: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        blurRadius: 7,
-                        offset: Offset(0,4),
-                        
-                      )
-                    ]
-                  ),
-              ),
-              Container(
-                child: Text('Horário de atendimento: 11h às 16h;\nEmissão de Senha: 10h30 às 15h30;'),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: Text('Tolerância: 3 Senhas'),
-              ),
-              
-              Container(
-                width: 320,
-                height: 1,
-                
-                margin: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        blurRadius: 7,
-                        offset: Offset(0,3),
-                        
-                      )
-                    ]
-                  ),
+      //color: Colors.blueAccent,
+      margin: const EdgeInsets.all(15),
+      alignment: Alignment.topCenter,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 320,
+            height: 1,
+            margin: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+            decoration: BoxDecoration(color: Colors.black, boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.7),
+                blurRadius: 7,
+                offset: Offset(0, 4),
               )
-            ],
-            )
-        );
+            ]),
+          ),
+          Container(
+            child: Text(
+                'Horário de atendimento: 11h às 16h;\nEmissão de Senha: 10h30 às 15h30;'),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Text('Tolerância: 3 Senhas'),
+          ),
+          Container(
+            width: 320,
+            height: 1,
+            margin: EdgeInsets.only(left: 5, right: 5, top: 10, bottom: 10),
+            decoration: BoxDecoration(color: Colors.black, boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.7),
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              )
+            ]),
+          )
+        ],
+      ));
 }
 
-Widget makeBoxCard(String letra, String numero, String quantidade){
-  return Container(
-                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.grey[100],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        blurRadius: 7,
-                        offset: Offset(0,3),
-                        
-                      )
-                    ]
-                  ),
-                  child: Row(
-                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                  Container(
-                    child: Text(letra),
-                    padding: EdgeInsets.fromLTRB(30, 10, 10, 10),
-                  ),
-                  Container(
-                    width: 1,
-                    height: 20,
-                    color: Colors.black,
-                    //padding: EdgeInsets.only(left: 5, right: 20, top: 10, bottom: 10),
-                  ),
-                  Container(
-                    child: Text(numero),
-                    padding: EdgeInsets.only(left: 10, right: 20, top: 10, bottom: 10),
-                  ),
-                  Container(
-                    child: Text(quantidade),
-                    padding: EdgeInsets.fromLTRB(140, 10, 0, 10),
-                  ),
-                  ],
+Widget makeBoxCard(String letra, String numero, String quantidade) {
+  return InkWell(
+      onTap: () => {
+            ticketNumber = int.parse(numero),
+            areaChoosen = letra,
+            //print('NUMERO: $ticketNumber\nAREA: $areaChoosen')
+          },
+      onLongPress: () => {
+            ticketNumber = null,
+            areaChoosen = null,
+            //print('NUMERO: $ticketNumber\nAREA: $areaChoosen')
+          },
+      child: Container(
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.grey[100],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.7),
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
                 )
-  );
-  
+              ]),
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                child: Text(letra),
+                padding: EdgeInsets.fromLTRB(30, 10, 10, 10),
+              ),
+              Container(
+                width: 1,
+                height: 20,
+                color: Colors.black,
+                //padding: EdgeInsets.only(left: 5, right: 20, top: 10, bottom: 10),
+              ),
+              Container(
+                child: Text(numero),
+                padding:
+                    EdgeInsets.only(left: 10, right: 20, top: 10, bottom: 10),
+              ),
+              Container(
+                child: Text(quantidade),
+                padding: EdgeInsets.fromLTRB(140, 10, 0, 10),
+              ),
+            ],
+          )));
 }
